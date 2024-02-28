eb72e0dbc2f3cbd19d8e00499ff77881d1dc2d53
Ayush Singh <aayush.singh2100@gmail.com>
2024-02-28 17:55:29 +0530

    added first task of feedback and contact section of landing page

---
+++ task_1_review_and_contact_components/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ContactSection from './components/ContactSection'
+import FeedbackSection from './components/FeedbackSection'
+
+function App() {
+
+  return (
+    <>
+      <FeedbackSection />
+      <ContactSection />
+    </>
+  )
+}
+
+export default App
